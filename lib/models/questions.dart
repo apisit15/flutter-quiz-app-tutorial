e7b628585fe930e079e4352c90b3ab6ce588f656
apisit15 <645051200428@mail.rmutk.ac.th>
2024-02-07 11:39:46 +0700

    first commit

---
+++ lib/models/questions.dart
@@ -0,0 +1,106 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: '1. โมเดล iPhone ใดที่มีการรองรับเทคโนโลยี 5G?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก.  iPhone 11',
+      'ข.  iPhone 12',
+      'ค.  iPhone XR',
+      'ง.  iPhone SE',
+    ],
+  ),
+  Question(
+    question: '2. สเปคการถ่ายภาพหลักของ iPhone 12 Pro Max ประกอบด้วยส่วนประกอบใดต่อไปนี้?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก. ไวด์ไอแม็กซ์ Ultra-Wide ขนาด 13 มิลลิเมตร',
+      'ข. กล้องหลัก 12 ล้านพิกเซล',
+      'ค. LiDAR Scanner',
+      'ง. ซูเปอร์รีติน่า',
+    ],
+  ),
+  Question(
+    question: '3. ไอโฟนรุ่นไหนที่มาพร้อมกับระบบ Face ID สำหรับการปลดล็อก?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก.  iPhone 8',
+      'ข.  iPhone SE',
+      'ค.  iPhone X',
+      'ง.  iPhone 7',
+    ],
+  ),
+  Question(
+    question: '4. iOS เวอร์ชันใดที่เปิดตัวพร้อมกับ iPhone 13 series?',
+    correctAnswerIndex: 3,
+    options: [
+      'ก.  iOS 14',
+      'ข.  iOS 12',
+      'ค.  iOS 13',
+      'ง.  iOS 15',
+    ],
+  ),
+  Question(
+    question: '5. iPhone รุ่นใดมีขนาดหน้าจอเล็กที่สุด?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก. iPhone 11 Pro',
+      'ข. iPhone XS',
+      'ค. iPhone 12 mini',
+      'ง. iPhone 8',
+    ],
+  ),
+  Question(
+    question: '6. ฟีเจอร์ใดที่เป็นเอกลักษณ์ของ Apple Watch ที่สามารถใช้งานร่วมกับ iPhone?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. ECG Monitor',
+      'ข. Siri',
+      'ค. Blood Oxygen Monitor',
+      'ง. Activity Rings',
+    ],
+  ),
+  Question(
+    question: '7. กล้องของ iPhone 12 Pro มีกล้องกี่ตัว?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. 2',
+      'ข. 3',
+      'ค. 4',
+      'ง. 5',
+    ],
+  ),
+  Question(
+    question: '8. iPhone รุ่นใดมีระบบการชาร์จไร้สาย MagSafe?',
+    correctAnswerIndex: 0,
+    options: [
+      'ก. iPhone 12',
+      'ข. iPhone 11 Pro',
+      'ค. iPhone XS',
+      'ง. iPhone SE',
+    ],
+  ),
+  Question(
+    question:
+        ' 9. ฟีเจอร์อะไรที่เป็นเอกลักษณ์ของ Siri บน iPhone?',
+    correctAnswerIndex: 2,
+    options: [
+      'ก. การแปลภาษา',
+      'ข. การส่งข้อความรูปแบบ Voice Memo',
+      'ค. การโทรออกด้วยเสียง',
+      'ง. การสแกนใบหน้า',
+    ],
+  ),
+  Question(
+    question:
+        ' 10. ฟีเจอร์ใดที่ช่วยให้การเลือกภาพที่ดีที่สุดในช่วงการถ่ายภาพ Burst Mode บน iPhone?',
+    correctAnswerIndex: 1,
+    options: [
+      'ก. Smart HDR',
+      'ข. Deep Fusion',
+      'ค. Night Mode',
+      'ง. QuickTake',
+    ],
+  ),
+];
